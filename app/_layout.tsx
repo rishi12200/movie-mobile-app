36696a0f46bc11c2c08221e822ddb027efaa2970
Rishi <rishi12200@gmail.com>
2025-12-08 01:15:39 +0530

    fix route changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,6 @@ export default function RootLayout() {
     <Stack.Screen name="(tabs)" options={{
       headerShown: false
     }} />
-    <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+    <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
   </Stack>;
 }
